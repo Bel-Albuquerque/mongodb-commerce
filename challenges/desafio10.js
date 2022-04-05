dd585964e544d8df1a981ec7e1ac76ea8aa6abd0
bel <mariaisabelalbuquerque@gmail.com>
2022-04-05 18:06:27 -0300

    corrige erros de lint do arquivo desafio10.js

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,4 +1,4 @@
 db.produtos.find({
-  valoresNutricionais: { $elemMatch: { tipo: "proteínas", percentual: { $lte: 40, $gte: 30 } } }
+  valoresNutricionais: { $elemMatch: { tipo: "proteínas", percentual: { $lte: 40, $gte: 30 } } },
 },
   { nome: 1, _id: 0 });
